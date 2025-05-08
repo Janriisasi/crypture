71f734b952fb35529b4118080de465c415ac754c
Sasi <johnreysasi55@gmail.com>
2025-05-08 11:00:29 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
