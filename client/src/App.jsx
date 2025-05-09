40262ac8a0eeda8eb03bcf81454c369c0301faa1
Sasi <johnreysasi55@gmail.com>
2025-05-09 20:16:23 +0800

    removed some unecessary codes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Toaster } from 'react-hot-toast';
 import Homepage from "./pages/homepage";
 
 function App() {
   return (
     <>
-    <Toaster 
-        position="top-right"
-        toastOptions={{
-          style: {
-            background: '#333',
-            color: '#fff',
-          },
-        }}
-      />
     <div className="App">
       <Homepage />
     </div>
