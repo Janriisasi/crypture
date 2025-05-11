91265164e99fc821967f7effe82201aa4fe0d9b8
Sasi <johnreysasi55@gmail.com>
2025-05-11 14:27:48 +0800

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import {AnimatePresence} from "framer-motion";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import Formpage from "./pages/formpage";
 
 function App() {
   return (
-    <>
-    <div className="App">
-      <Homepage />
-    </div>
-    </>
+    <AnimatePresence mode={"wait"}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/form" element={<Formpage />} />
+      </Routes>
+    </Router>
+    </AnimatePresence>
   );
 }
 
