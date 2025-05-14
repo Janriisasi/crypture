8755bff671902d9d8ac6d4fbb9b5230da56c500c
Sasi <johnreysasi55@gmail.com>
2025-05-14 11:06:03 +0800

    fixed navigation and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import {AnimatePresence} from "framer-motion";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/loginpage";
 import Homepage from "./pages/homepage";
 import Formpage from "./pages/formpage";
 
-function App() {
+//Protected Route
+const ProtectedRoute = ({ children }) => {
+  // Check if user is logged in
+  const isAuthenticated = localStorage.getItem('user') !== null;
+  
+  // If not authenticated, redirect to login
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+  
+  // If authenticated, error
+  return children;
+};
+
+const App = () => {
   return (
     <AnimatePresence mode={"wait"}>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/form" element={<Formpage />} />
+        <Route 
+          path="/homepage" 
+          element={
+            <ProtectedRoute>
+              <Homepage />
+              <Formpage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="*" element={<Navigate to="/homepage" />} />
       </Routes>
     </Router>
     </AnimatePresence>
