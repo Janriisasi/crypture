fc7743b158cb72e2996723e271a74eb5efd3b87c
Sasi <johnreysasi55@gmail.com>
2025-05-16 18:29:34 +0800

    added a tailwindcss and postcss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' // or whatever framework you're using
+import postcssConfig from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [postcssConfig, autoprefixer],
+    },
+  },
 })
