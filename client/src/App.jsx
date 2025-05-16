64c51061307488457e78ee79277d4b5040b180d9
Sasi <johnreysasi55@gmail.com>
2025-05-16 17:07:05 +0800

    removed the login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,26 @@
 import React from "react";
 import {AnimatePresence} from "framer-motion";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Login from "./pages/loginpage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Formpage from "./pages/formpage";
-
-// Custom query param handler
-const HomepageWithToast = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const action = queryParams.get('action');
-  
-  return <Homepage initialAction={action} />;
-};
-
-//Protected Route
-const ProtectedRoute = ({ children }) => {
-  // Check if user is logged in
-  const isAuthenticated = localStorage.getItem('user') !== null;
-  
-  // If not authenticated, redirect to login
-  if (!isAuthenticated) {
-    return <Navigate to="/login" />;
-  }
-  
-  // If authenticated, render children
-  return children;
-};
 
 const App = () => {
   return (
     <AnimatePresence mode={"wait"}>
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
         <Route 
           path="/homepage" 
           element={
-            <ProtectedRoute>
-              <HomepageWithToast />
-            </ProtectedRoute>
+              <Homepage />
           } 
         />
         <Route 
           path="/formpage" 
           element={
-            <ProtectedRoute>
               <Formpage />
-            </ProtectedRoute>
           } 
         />
-        <Route path="*" element={<Navigate to="/homepage" />} />
       </Routes>
     </Router>
     </AnimatePresence>
